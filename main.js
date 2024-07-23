8ecc334b96521cf4e88141003b3d690bb8c7c0de
David Hage <davhage@icloud.com>
2024-07-22 19:19:14 -0600

    second commit

--- main.js
+++ main.js
@@ -1,33 +1,3 @@
-/*let playerScore = 0;
-let computerScore = 0;
-let userOption = prompt(`Selecciona piedra, papel o tijera`).toLowerCase();
-
-const computerPlay = () => {
-    const options = ['piedra', 'papel', 'tijera'];
-    let choice = options[Math.floor(Math.random() * options.length)];
-    return choice;
-}
-
-const playRound = (playerSelection) => {
-    let computerSelection = computerPlay();
-
-    if((playerSelection === 'tijera' && computerSelection == 'papel') || 
-       (playerSelection === 'papel' && computerSelection == 'piedra') ||
-       (playerSelection === 'piedra' && computerSelection == 'tijera')) {
-            playerScore += 1;
-            console.log(`Ganaste! ${playerScore} - computadora ${computerScore}`);
-    } else if(playerSelection === computerSelection) {
-        console.log(`Empate`);
-    } else {
-        computerScore += 1;
-        console.log(`Perdiste! tu ${playerScore} - computadora ${computerScore}`);
-    }
-} 
-console.log(playRound(userOption))
-console.log(computerPlay());
-console.log(userOption);
-*/
-
 let playerScore = 0;
 let computerScore = 0;
 
